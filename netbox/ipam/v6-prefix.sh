0483949c642564c1a3ed4d8a7ca5ccd09bf1e451
aibix <aibix@aibix.io>
2024-03-23 00:04:59 +0100

    korrektur

--- netbox/ipam/v6-prefix.sh
+++ netbox/ipam/v6-prefix.sh
@@ -4,7 +4,8 @@ for i in {0..7}
 do
     curl --insecure \
 	 -H "Authorization: Token 29807a042ce4bc270bcdc736a1305793d2ee401f" \
-	 -H "Content-Type: application/json" -H "Accept: application/json; indent=4" \
+	 -H "Content-Type: application/json" \
+	 -H "Accept: application/json; indent=4" \
 	 https://192.168.111.205/api/ipam/prefixes/ \
 	 -X POST --data "{\"prefix\": \"2001:470:731b:4${i}00::/56\", \"status\": \"active\", \"tags\": [1], \"tenant\": 1, \"site\": 2}"
 done
