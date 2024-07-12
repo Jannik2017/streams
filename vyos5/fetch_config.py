36a4dfe41cf0f4fe05f69e0ca5b68a1618dcbcee
aibix <aibix@aibix.io>
2024-07-12 11:21:54 +0200

    works :)

--- vyos5/fetch_config.py
+++ vyos5/fetch_config.py
@@ -26,15 +26,37 @@ def fetch_vyos_config(task: Task) -> Result:
 def main():
     nr = InitNornir(config_file="config.yml")
     result = nr.run(task=fetch_vyos_config)
-    print(type(result))
+    print("type(result): ", type(result))
     print("result: ", result)
-    for host, task_results in result.items():
-        print(host)
-        print(task_results)
-        print("1: ", task_results[1])
-        for task_result in task_results[1].result.split('\n'):
-            print(task_result)
-            print(f"{host}: {task_result}")
+#    for host, task_results in result.items():
+#        for task_result in task_results[1].result.split('\n'):
+#            print(task_result)
+#            print(f"{host}: {task_result}")
+
+    for hostname, multi_result in result.items():
+        print('=================', hostname, '=================')
+        for task_result in multi_result:
+            # Decomposing the task_result
+            host = task_result.host
+            name = task_result.name
+            task_output = task_result.result
+            failed = task_result.failed
+            changed = task_result.changed
+            severity_level = task_result.severity_level
+            diff = task_result.diff
+            exception = task_result.exception
+            #traceback = task_result.traceback
+
+            print(f"Host: {host}")
+            print(f"Task Name: {name}")
+            print(f"Result: {task_output}")
+            print(f"Failed: {failed}")
+            print(f"Changed: {changed}")
+            print(f"Severity Level: {severity_level}")
+            print(f"Diff: {diff}")
+            print(f"Exception: {exception}")
+            #print(f"Traceback: {traceback}")
+
 
 if __name__ == "__main__":
     main()
