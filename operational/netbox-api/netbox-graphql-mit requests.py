d442d8e269c355ad88976e75959005c79879f573
aibix <aibix@aibix.io>
2024-09-28 00:18:07 +0200

    interfaces aller router holen

--- operational/netbox-api/netbox-graphql-mit requests.py
+++ operational/netbox-api/netbox-graphql-mit requests.py
@@ -14,43 +14,43 @@ headers = {
 }
 
 url = f"{netbox_url}/graphql/"
-query_device_interfaces = '''
-query {
-    device_list(filters: {name: {exact: "p1r7v"}}) {
-        name
-        interfaces {
+
+def query_device_interfaces_function(device):
+    query_device_interfaces = '''
+    query {
+        device_list(filters: {name: {exact: "''' + device + '''"}}) {
             name
-            ip_addresses {
-                address
+            interfaces {
+                name
+                ip_addresses {
+                    address
+                    tags {
+                        name
+                    }
+                }
                 tags {
                     name
-                }
+                }  
             }
-            tags {
-                name
-            }  
+            custom_fields
         }
-        custom_fields
     }
-}
-'''
+    '''
+    return query_device_interfaces
+    #return device
 
-query_config_templates = '''
+query_devices_p1 = '''
 query {
-    config_template(id: 1) {
-        template_code
+    device_list(filters: {name: {starts_with: "p1r"}}) {
+        name
     }
 }
 '''
 
 # get data from netbox
-response_device_interfaces = requests.post(url, json={"query": query_device_interfaces}, headers=headers, verify=False)
-response_config_templates = requests.post(url, json={"query": query_config_templates}, headers=headers, verify=False)
+response_devices_p1 = requests.post(url, json={"query": query_devices_p1}, headers=headers, verify=False)
 
-# print ip address information from query_device_interfaces
-pprint(response_device_interfaces.json())
+for device in response_devices_p1.json()['data']['device_list']:
+    response_device_interfaces = requests.post(url, json={"query": query_device_interfaces_function(device['name'])}, headers=headers, verify=False)
+    pprint(response_device_interfaces.json())
 
-# # print config template
-# lines_config_templates = response_config_templates.json()['data']['config_template']['template_code'].split('\r\n')
-# for line in lines_config_templates:
-#     print(line)
