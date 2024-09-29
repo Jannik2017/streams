2622c94e8b88c251b14da6ac7b7e79fae5cde00d
aibix <aibix@aibix.io>
2024-09-29 15:23:46 +0200

    bgp config bauen

--- operational/netbox-api/netbox-graphql-mit requests.py
+++ operational/netbox-api/netbox-graphql-mit requests.py
@@ -1,5 +1,4 @@
 from pprint import pprint
-import json
 import requests
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
@@ -15,7 +14,7 @@ headers = {
 
 url = f"{netbox_url}/graphql/"
 
-def query_device_interfaces_function(device):
+def query_device_interfaces(device):
     query_device_interfaces = '''
     query {
         device_list(filters: {name: {exact: "''' + device + '''"}}) {
@@ -37,7 +36,7 @@ def query_device_interfaces_function(device):
     }
     '''
     return query_device_interfaces
-    #return device
+
 
 query_devices_p1 = '''
 query {
@@ -50,7 +49,52 @@ query {
 # get data from netbox
 response_devices_p1 = requests.post(url, json={"query": query_devices_p1}, headers=headers, verify=False)
 
+# print result
+bgp_config = {}
 for device in response_devices_p1.json()['data']['device_list']:
-    response_device_interfaces = requests.post(url, json={"query": query_device_interfaces_function(device['name'])}, headers=headers, verify=False)
-    pprint(response_device_interfaces.json())
+    device_interfaces = requests.post(url, json={"query": query_device_interfaces(device['name'])}, headers=headers, verify=False)
+    
+    print("Device: " + device['name'])
+    bgp_config[device['name']] = {}
+    for interface in device_interfaces.json()['data']['device_list'][0]['interfaces']:
+        if len(interface['ip_addresses']) > 0:
+            for ip in interface['ip_addresses']:
+                for tag in ip['tags']:
+                    if tag['name'].startswith('route-reflector'):
+                        print("Interface: " + ip['address'] + " is " + tag['name'])
+                        bgp_config[device['name']]['rr-type'] = tag['name']
+                        bgp_config[device['name']]['ipv4-address'] = ip['address']
+                        bgp_config[device['name']]['asn'] = device_interfaces.json()['data']['device_list'][0]['custom_fields']['BGP_ASN']
+
+route_reflectors = []
+route_reflector_clients = []
+
+# This loop iterates through the routers and their configurations.
+for router, config in bgp_config.items():
+    if 'rr-type' in config and config['rr-type'] == 'route-reflector':
+        route_reflectors.append([router, config['ipv4-address']])
+    else:
+        route_reflector_clients.append([router, config['ipv4-address']])
+    
+# This loop iterates through the route reflectors and their clients, printing each client for each route reflector.
+for route_reflector in route_reflectors:
+    print(f"Route Reflector {route_reflector[0]} has the following clients:")
+    for client in route_reflector_clients:
+        config_lines = f"""
+        set protocols bgp neighbor { client[1] } address-family ipv4-unicast nexthop-self
+        set protocols bgp neighbor { client[1] } description '{ client[0]}'
+        set protocols bgp neighbor { client[1] } remote-as '{ bgp_config[client[0]]['asn'] }'
+        set protocols bgp neighbor { client[1] } update-source 'lo'"""
+
+        print(config_lines)
+
+for client in route_reflector_clients:
+    for route_reflector in route_reflectors:
+        config_lines = f"""
+        set protocols bgp neighbor { route_reflector[1] } address-family ipv4-unicast nexthop-self
+        set protocols bgp neighbor { route_reflector[1] } description '{ route_reflector[0]}'
+        set protocols bgp neighbor { route_reflector[1] } remote-as '{ bgp_config[route_reflector[0]]['asn'] }'
+        set protocols bgp neighbor { route_reflector[1] } update-source 'lo'"""
+
+        print(client[0], config_lines)
 
