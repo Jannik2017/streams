d02c10e5e198f79ec85eec610962ed0640d276cb
aibix <aibix@aibix.io>
2025-02-21 14:34:53 +0100

    proxmox

---
+++ proxmox/proxy.sh
@@ -0,0 +1,5 @@
+export http_proxy="http://194.164.48.10:3128/"
+export https_proxy="http://194.164.48.10:3128/"
+export ftp_proxy="http://194.164.48.10:3128/"
+export no_proxy="127.0.0.1,localhost"
+
